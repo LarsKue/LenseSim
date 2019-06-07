35a3285ddb80969bc8fd16fec6e75c65a4153283
Lars Kue <lars@kuehmichel.de>
2019-06-07 22:29:02 +0200

    More Settings

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,12 @@ constexpr int window_width = 1920;
 constexpr int window_height = 1080;
 constexpr double aspect_ratio = static_cast<double>(window_width) / window_height;
 constexpr int anti_aliasing_level = 8;
+constexpr int window_flags = sf::Style::Close;
 
 int main() {
     sf::ContextSettings settings;
     settings.antialiasingLevel = anti_aliasing_level;
-    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "LenseSim", sf::Style::Default, settings);
+    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "LenseSim", window_flags, settings);
     sf::CircleShape shape(100.f, 100);
     shape.setFillColor(sf::Color::Green);
 
