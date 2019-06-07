ab045bf1edc1008f968062c9c93cc958d823a216
Lars Kue <lars@kuehmichel.de>
2019-06-07 22:26:38 +0200

    Added Some Basic Settings

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,16 @@
 
 #include <SFML/Graphics.hpp>
 
+constexpr int window_width = 1920;
+constexpr int window_height = 1080;
+constexpr double aspect_ratio = static_cast<double>(window_width) / window_height;
+constexpr int anti_aliasing_level = 8;
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = anti_aliasing_level;
+    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "LenseSim", sf::Style::Default, settings);
+    sf::CircleShape shape(100.f, 100);
     shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen()) {
